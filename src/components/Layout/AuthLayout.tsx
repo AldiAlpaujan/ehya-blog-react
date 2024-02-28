8475e5b6c2221408c5a0654735d88cd509b50857
AldiAlpaujan <aldi.m.alpaujan.2@gmail.com>
2024-02-28 08:01:42 +0700

    update

--- src/components/Layout/AuthLayout.tsx
+++ src/components/Layout/AuthLayout.tsx
@@ -11,8 +11,8 @@ const AuthLayout = (props: AuthLayoutProps) => {
     return (
         <PageLayout className="h-full">
             <div className="container h-full sm:flex sm:justify-center">
-                <form onSubmit={props.onSubmit} className="flex flex-col justify-center items-center h-[100%] w-full
-                    sm:max-w-sm" >
+                <form onSubmit={props.onSubmit} className="flex flex-col justify-center items-center h-[90%] w-full
+                    sm:max-w-sm lg:h-[100%]" >
                     <h1 className="text-2xl font-roboto font-bold text-dark-soft mb-7
                     md:text-3xl lg:text-4xl" >
                         {props.title}
